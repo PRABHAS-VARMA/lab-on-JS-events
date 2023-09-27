2bbda598bb4a17c8b4a082336d4e3dad3c4dedcd
PRABHAS-VARMA <141707979+PRABHAS-VARMA@users.noreply.github.com>
2023-09-27 20:17:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lab-on-JS-events
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-fq9yhq)
---
+++ script.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+  var spanUser = document.getElementById("user")
+  spanUser.innerHTML= "Prabhas Varma"
+};
